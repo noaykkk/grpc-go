3f6241afff2d5132f94c1807a1e4136449438b4d
noaykkk <idnoay@gmail.com>
2023-05-11 05:40:58 -0500

    Config server with ping-pong

--- server/main.go
+++ server/main.go
@@ -1,4 +1,27 @@
 package server
 
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"net"
+)
+
 type server struct {
+	hello_grpc.UnimplementedHelloGRPCServer
+}
+
+func (s *server) SayHi(ctx context.Context, req *hello_grpc.Req) (res *hello_grpc.Res, err error) {
+	fmt.Println(req.GetMessage())
+	return &hello_grpc.Res{Message: "I am message"}, nil
+}
+
+func main() {
+	listen, err := net.Listen("rpc", "8000")
+	if err != nil {
+		return
+	}
+	s := grpc.NewServer()
+	hello_grpc.RegisterHelloGRPCServer(s, &server{})
+	s.Serve(listen)
 }
